515d9f25b50a134484fe800f413f7dca35d19032
sung <salamander011017@gmail.com>
2024-06-27 22:15:06 +0900

    B15686

--- src/main/java/JavaCodingTestStudy/week4/B15686.java
+++ src/main/java/JavaCodingTestStudy/week4/B15686.java
@@ -1,61 +1,9 @@
 package JavaCodingTestStudy.week4;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class B15686 {
-    static int [][]map;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()); // N x N 맵을 받기 위함
-        int M = Integer.parseInt(st.nextToken());// 망하지 않는 최대 치킨집 수
-        map = new int[N][N];
-        ArrayList<RowCol>chick = new ArrayList<>(); //치킨집의 정보를 가진 배열
-        ArrayList<RowCol>citizen = new ArrayList<>();
-        int k = 0;
-        int p = 0;
-        for(int i = 0 ; i < N ; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j = 0 ; j < N ; j++){
-                map[i][j] = Integer.parseInt(st.nextToken());
-                if(map[i][j] == 2){
-                    chick.add(new RowCol(j,i));
-                    k++;
-                }else if(map[i][j] == 1){
-                    citizen.add(new RowCol(j,i));
-                }
-            }
-        }
-        int length[] = new int[chick.size()];
-        for(int i = 0; i<chick.size(); i++){
-            for(int j = 0 ; j < citizen.size() ; j++){
-                length[i] += Math.abs(chick.get(i).row-citizen.get(j).row)+Math.abs(chick.get(i).col-citizen.get(j).col);
-            }
-        }
-        RowCol realChick[] = new RowCol[M];
-        for(int i = 0 ; i < M ; i++){
-            realChick[i] = chick.get(i);
-        }
-        int minLength[] = new int[M];
-        int min = Integer.MAX_VALUE;
-        int sum = 0;
-        for(int i = 0; i<citizen.size(); i++){
-            for(int j = 0 ; j < M ; j++){
-                int temp = Math.abs(citizen.get(i).row - realChick[j].row) + Math.abs(citizen.get(i).col - realChick[j].col);
-                if(temp < min){
-                    min = temp;
-                }
-            }
-            sum += min;
-        }
-        System.out.println(sum);
-    }
-}
 class RowCol{
     int row,col;
     public RowCol(int row, int col){
@@ -63,3 +11,74 @@ class RowCol{
         this.col = col;
     }
 }
+public class B15686 {
+
+    static int M,N;
+    static int [][]map;
+    static  ArrayList<RowCol>chick;
+    static  ArrayList<RowCol>citizen;
+    static int answer;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        N = Integer.parseInt(st.nextToken()); // N x N 맵을 받기 위함
+        M = Integer.parseInt(st.nextToken());// 망하지 않는 최대 치킨집 수
+
+        map = new int[N][N];
+       chick = new ArrayList<>(); //치킨집의 정보를 가진 배열
+       citizen = new ArrayList<>();
+
+        for(int i = 0 ; i < N ; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j = 0 ; j < N ; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+
+                if(map[i][j] == 2){
+                    chick.add(new RowCol(i,j));
+                }else if(map[i][j] == 1){
+                    citizen.add(new RowCol(i,j));
+                }
+            }
+        }
+
+        answer = Integer.MAX_VALUE;
+        visited = new boolean[chick.size()];
+
+        DFS(0,0);
+        bw.write(answer + "\n");
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+    public static void DFS(int start, int count){
+        if(count == M) {
+            int res = 0; //현재 선택된 치킨집에서 모든 집까지의 최소 거리의 합
+
+            for (int i = 0; i < citizen.size(); i++) { //시민의 집에서 치킨집까지의 거리를 측정
+                int temp = Integer.MAX_VALUE;
+                for (int j = 0; j < chick.size(); j++) {
+                    if (visited[j]) { //현재 선택된 치킨집임
+                        int distance = Math.abs(citizen.get(i).row - chick.get(j).row)
+                                + Math.abs(citizen.get(i).col - chick.get(j).col);
+                        temp = Math.min(temp, distance);//한 시민의 집에서 각 치킨집 까지의 가장 작은 거리를 구함
+                    }
+                }
+                res += temp; //한 집에서 가장 적은 치킨집의 거리를 구해서 더함
+            }
+            answer = Math.min(res, answer);//만약 더 짧은 거리가 있다면 answer의 값을 변경
+            return;
+        }
+            for(int p  = start; p < chick.size(); p++){
+                visited[p] = true;
+                DFS(p+1,count+1);
+                visited[p] = false;
+            }
+
+    }
+}
+
